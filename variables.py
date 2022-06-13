dd5ea8b4b29f6a9b80f3cd2107936810f5d3e6ef
atakkant <atakan.tuncer@gmail.com>
2022-06-13 20:35:41 +0300

    first commit

--- variables.py
+++
@@ -1,4 +0,0 @@
-URLS = ['http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/meta_Amazon_Instant_Video.json.gz','http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/meta_Musical_Instruments.json.gz']
-SAMPLE_JSONFILE = 'sample_meta_Musical_Instruments.json'
-SEC_SAMPLE_JSONFILE = 'sample2_Musical_Instruments.json'
-GZ_PATH = 'meta_Amazon_Instant_Video.json.gz'
